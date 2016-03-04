a2e61aca1fa58f9497236e23f6dd8c2eab6142c1
西伯利亚游牧人 <nomadcube@gmail.com>
2016-03-04 16:43:20 +0800

    直接构造哈希函数而不是将值和值域存入哈希表中

--- test.py
+++ test.py
@@ -1,11 +1,27 @@
+import re
+from time import time
+from random import randint
+
 import minhashing
 
+with open('/Users/wumengling/PycharmProjects/cpp_swig_test/dat_test.txt', 'w') as out_f:
+    size = 10000
+    s1 = ''.join([chr(n) for n in [randint(97, 107) for _ in range(size)]])
+    s2 = ''.join([chr(n) for n in [randint(97, 107) for _ in range(size)]])
+    s1_new = re.sub("a", " ", s1)
+    s2_new = re.sub("a", " ", s2)
+    out_f.write(s1_new + '\n')
+    out_f.write(s2_new + '\n')
 
-a = minhashing.read("/Users/wumengling/PycharmProjects/cpp_swig_test/dat_test")
+a = minhashing.read("/Users/wumengling/PycharmProjects/cpp_swig_test/dat_test.txt")
 print(a)
 
-sig_a = minhashing.get_signature(a, 10)
-print sig_a
-
+sig_size = 100
+t = time()
+sig_a = minhashing.get_signature(a, sig_size)
 print minhashing.jaccard_similarity(sig_a)
+print "min hashing time: " + repr(time() - t)
+
+t = time()
 print minhashing.jaccard_similarity(a[0], a[1])
+print "old algorithm time: " + repr(time() - t)
